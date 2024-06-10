887b982d5d9a7fe376d224d9e2cf11c14832feae
Celio Hira <cehira@gmail.com>
2024-06-10 15:49:40 -0300

    setting new version (0.3.0)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 with io.open('README.md', mode='r') as doc:
     LONG_DESCRIPTION = doc.read()
     
-VERSION = '0.2.0'
+VERSION = '0.3.0'
 
 setup(
     name="pyspark-connectors",
